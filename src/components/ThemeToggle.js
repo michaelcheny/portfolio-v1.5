a1569d62b7d8315e5392befa27df43ee045a2f13
Michael Chen <michaelchengaming@gmail.com>
2020-07-05 03:24:59 -0700

    add hover and diff stle to toggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -6,11 +6,17 @@ const Toggle = styled.button`
   right: 0;
   top: 0;
   border-radius: 5px;
-  padding: 1px 3px;
+  padding: 0.3rem 0.85rem;
   margin: 6px;
   background-color: ${(props) => props.theme.ternary};
   color: ${(props) => props.theme.textPrimary};
   border: none;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.15);
+    background-color: ${(props) => props.theme.secondary};
+  }
 `;
 
 const ThemeToggle = ({ toggle }) => {
