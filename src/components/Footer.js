b7ab4f78beb0c71148af3ab090defa2987a7ecb5
Michael Chen <michaelchengaming@gmail.com>
2020-07-05 20:05:12 -0700

    add icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub, faMedium, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-const Foot = styled.footer`
+const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 5.5vh;
+  padding: 2rem 1rem;
   background-color: ${(props) => props.theme.primary};
 `;
 
-const Ul = styled.ul`
-  list-style: none;
-`;
-
-const Li = styled.li`
+const Item = styled.a`
   display: inline-block;
   margin: 0 1rem;
-  /* color: ${(props) => props.theme.textPrimary}; */
+  color: ${(props) => props.theme.textPrimary};
+  transition: transform 300ms;
+  &:hover {
+    color: ${(props) => props.theme.textSecondary};
+    transform: scale(1.3);
+  }
 `;
 
 const Footer = () => {
   return (
-    <Foot>
-      <Ul>
-        <Li>item</Li>
-        <Li>uten</Li>
-        <Li>item</Li>
-        <Li>imte</Li>
-      </Ul>
-    </Foot>
+    <Container>
+      <Item href="mailto:michaelchengaming@gmail.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faEnvelope} size="2x" />
+      </Item>
+      <Item href="https://www.linkedin.com/in/michaelchen13/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </Item>
+      <Item href="https://github.com/michaelcheny" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </Item>
+      <Item href="https://medium.com/@michaelychen" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faMedium} size="2x" />
+      </Item>
+      <Item
+        href="https://www.youtube.com/channel/UCsVPeLiZClEYtyPHfweRniw?view_as=subscriber"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </Item>
+    </Container>
   );
 };
 
