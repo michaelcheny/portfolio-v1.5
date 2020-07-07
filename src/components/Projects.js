b4bceae0d64bd6bd3a4146965ed5cecb37ed4334
Michael Chen <michaelchengaming@gmail.com>
2020-07-06 20:39:25 -0700

    add placeholder image of project and make it responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import data from "../assets/data/data.json";
 
 const Section = styled.section`
-  height: 100vh;
+  /* height: 100vh; */
   background-color: ${(props) => props.theme.primary};
   color: ${(props) => props.theme.textPrimary};
   display: flex;
@@ -15,13 +15,80 @@ const Section = styled.section`
 `;
 
 const ProjectContainer = styled.div`
-  /* display: flex;
-  flex-direction: column; */
+  display: flex;
+  margin: 2rem 0;
   border: blue 1px solid;
+  width: 70vw;
+  max-width: 1024px;
+  height: 40vh;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  max-width: 50%;
+  justify-content: center;
+
+  .title {
+    font-size: 2rem;
+    color: ${(props) => props.theme.textAccent};
+  }
+  .description {
+    color: ${(props) => props.theme.textSecondary};
+    background-color: ${(props) => props.theme.secondary};
+    padding: 1.25rem;
+    margin: 1rem 0;
+    border-radius: 2px;
+  }
+`;
+
+const StackContainer = styled.div`
+  margin-top: 1rem;
+  span {
+    /* background-color: ${(props) => props.theme.ternary}; */
+    color: ${(props) => props.theme.textAccent};
+    /* padding: 5px 10px; */
+    font-size: 15px;
+    border-radius: 3px;
+    margin: 1rem 0;
+    margin-right: 1rem;
+  }
+`;
+
+const PhotoContainer = styled.div`
+  width: 50%;
+  border: 1px red solid;
+  display: flex;
+  justify-content: center;
+  .project-image {
+    object-fit: scale-down;
+    max-width: 100%;
+    max-height: auto;
+  }
 `;
 
 const Project = ({ project }) => {
-  return <ProjectContainer>{project.name}</ProjectContainer>;
+  return (
+    <ProjectContainer>
+      <DetailContainer>
+        <span className="title">{project.name}</span>
+        <div className="description">{project.description}</div>
+        <StackContainer>
+          {project.techStack.map((stack) => (
+            <span>{stack}</span>
+          ))}
+        </StackContainer>
+      </DetailContainer>
+      <PhotoContainer>
+        <img src={project.image} alt="temp" className="project-image" />
+      </PhotoContainer>
+    </ProjectContainer>
+  );
 };
 
 const Projects = () => {
