1743094a61b16638385debf64243e339c4a41b48
Michael Chen <michaelchengaming@gmail.com>
2020-07-08 15:03:30 -0700

    add about section with few lines of stuff

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,51 @@ const AboutSection = styled.section`
   justify-content: center;
   align-items: center;
 `;
+const AboutContainer = styled.div`
+  display: flex;
+  max-width: 1024px;
+  border: 1px pink solid;
+  width: 70%;
+`;
+const Description = styled.div`
+  border: 1px pink solid;
+  display: flex;
+  flex-direction: column;
+  width: 55%;
+  p {
+    padding: 0.5rem;
+  }
+`;
+const Skills = styled.div`
+  border: 1px pink solid;
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+`;
 
 const About = () => {
-  return <AboutSection>about</AboutSection>;
+  return (
+    <AboutSection>
+      <AboutContainer>
+        <Description>
+          <h2>About Me</h2>
+          <p>My name is Michael Chen and I'm a software engineer based in San Francisco, Ca.</p>
+          <p>I am a Flatiron School alumni who loves to build things for the web. I specialize in</p>
+          <p>
+            When I'm not clacking away on my keyboard, I can be found reading countless tech articles,
+            learning new skills, or playing with my dog.
+          </p>
+          <p>
+            Hobbies include: Fishing, powerlifting, video games, cooking, and keyboards(I know.. weird
+            right?).
+          </p>
+        </Description>
+        <Skills>
+          <h2>Skills</h2>
+        </Skills>
+      </AboutContainer>
+    </AboutSection>
+  );
 };
 
 export default About;
